c8618443c56d81f4d1da1bfd81dda271faea07d2
Om Verma <omverma@gmail.com>
2026-04-02 01:27:48 +0530

    fix: convert modules from submodule to normal folder

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-module "main_vpc" {
+module "vpc" {
   source = "./modules/vpc"
-vpc_cidr  = var.vpc_cidr
+
+  vpc_cidr            = "10.0.0.0/15"
+  public_subnet_cidr  = "172.16.0.1/20"
+  private_subnet_cidr = "172.16.0.0/20"
 }
 
-module "ec2-instance" {
+module "ec2" {
   source = "./modules/ec2"
 
-  image_id      = var.image_id
-  instance_type = var.instance_type
-  key_pair      = var.key_pair
+  image_id      = "ami-0ec10929233384c7f"
+  instance_type = "t2.micro"
+  key_pair      = "nv-new"
 
-  vpc_id        = module.main_vpc.vpc_id
-  subnet_id     = module.main_vpc.public_subnets_cidr
+  vpc_id    = module.main_vpc.vpc_id
+  subnet_id = module.main_vpc.public_subnet_id
 }
